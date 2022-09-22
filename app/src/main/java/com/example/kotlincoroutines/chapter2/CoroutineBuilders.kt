a064b31ce0287653656763ca6ca4078e8cf6d132
ShroukMamdouhAbdElAziz <104698688+ShroukMamdouhAbdElAziz@users.noreply.github.com>
2022-09-22 23:15:43 +0200

    Chapter2 overview

---
+++ app/src/main/java/com/example/kotlincoroutines/chapter2/CoroutineBuilders.kt
@@ -0,0 +1,2 @@
+package com.example.kotlincoroutines.chapter2
+
--- app/src/main/java/com/example/kotlincoroutines/chapter2/MainPoints.kt
+++ app/src/main/java/com/example/kotlincoroutines/chapter2/MainPoints.kt
@@ -20,4 +20,73 @@ package com.example.kotlincoroutines.chapter2
 
   3.   runBlocking
 
+
+  what is the significant of using the companion Object GlobalScope with launch{}
+  -------------------------------------------------------------------------------
+ At the beginning note that
+ ============================
+   you  have a LoginScreen and you have a coroutine inside it using launch{}, this launch fun create a coroutine in the LOCAL scope
+   and navigate from loginScreen to home
+   Screen which have coroutine inside it .
+
+   and when you navigated back from Homescreen to the previous screen (Login screen), the homescreen will be destroyed
+   and hence the coroutine will be canceled .( if the lifeTime of the screen is over , then coroutine inside this
+   screen is over).
+
+   So, What about using GlobalScope with launch{}
+   ================================================
+   GlobalScope.launch{} -> // create coroutines at global(app) level.
+
+   Global Coroutines are Top-Level coroutines and it can survive the entire life of the application.so even
+   all the local scopes are destroyed , then also the global coroutine no matter from from which local scope it was
+   launched. it can survive the entire life of the app.
+
+
+   When should you launch coroutine Globally and when should launch coroutine locally
+   -------------------------------------------------------------------------------------
+
+   1. You can use GlobalScope( create coroutine at global(App) level),
+
+    ===================================================================
+    in cases where you donot depend on the local scopes.such as , once you click on a button to download a file
+    , then it can run in the global scope and let it complete in the background.Because
+    Generally the downloaded file is not needed during your ongoing activities in your application.
+    just start your downloaded operation and move on with your other work.
+
+    tHe same is the case of playing music, just start playing the music and it has nothing
+    to do with your other ongoing task.
+
+     GlobalScope.launch{
+       ex : download file
+       ex: play music
+     }
+
+   2. You can create coroutine at local scope
+   ===========================================
+
+   when you want to compute some values within alocal scope and end it there only.
+   or you can do some login , and you need to end that login operation right there
+
+   launch{}
+
+
+   Highly note that
+   ====================
+   also note that using GlobalScope.launch is highly discouraged , only use it when it is needed.
+   Because , if you create a coroutine globally and you forget about it , then it will keep running
+   in the background and consume a lot of memory.which is not good actually.
+   so you need to use a global coroutine very very carefully.
+
+          IN Short : using LAUNCH function should be your default choice.
+
+
+          ex:
+
+          launch{
+            // some data computation
+            //Login Operation
+          }
+
+
+
  */
